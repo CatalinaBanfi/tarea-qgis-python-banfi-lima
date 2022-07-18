eeac239568cf1c4383fac764a69ac5bd7c835193
Matías Lima <71034029+limamatias@users.noreply.github.com>
2022-07-18 15:56:01 -0300

    Add files via upload
    Agrego el código de Python del modelo 1

---
+++ files/model1.py
@@ -0,0 +1,132 @@
+"""
+Model exported as python.
+Name : model1
+Group : 
+With QGIS : 32208
+"""
+
+from qgis.core import QgsProcessing
+from qgis.core import QgsProcessingAlgorithm
+from qgis.core import QgsProcessingMultiStepFeedback
+from qgis.core import QgsProcessingParameterFeatureSink
+import processing
+
+
+class Model1(QgsProcessingAlgorithm):
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterFeatureSink('Autoinc_id', 'autoinc_id', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Wldsout', 'wldsout', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Length', 'length', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Field_calc', 'field_calc', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Output_menor_a_11', 'OUTPUT_menor_a_11', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Fix_geo', 'fix_geo', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+
+    def processAlgorithm(self, parameters, context, model_feedback):
+        # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
+        # overall progress through the model
+        feedback = QgsProcessingMultiStepFeedback(6, model_feedback)
+        results = {}
+        outputs = {}
+
+        # Field calculator
+        alg_params = {
+            'FIELD_LENGTH': 2,
+            'FIELD_NAME': 'length',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 1,  # Integer
+            'FORMULA': 'length(NAME_PROP)',
+            'INPUT': 'Incremented_abf8be7e_164f_45cc_ae81_25cd53378841',
+            'OUTPUT': parameters['Length']
+        }
+        outputs['FieldCalculator'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Length'] = outputs['FieldCalculator']['OUTPUT']
+
+        feedback.setCurrentStep(1)
+        if feedback.isCanceled():
+            return {}
+
+        # Fix geometries
+        alg_params = {
+            'INPUT': 'C:/Maestría UdeSA/Materias UdeSA/Herramientas computacionales/4. Python + GIS/input/langa/langa.shp',
+            'OUTPUT': parameters['Fix_geo']
+        }
+        outputs['FixGeometries'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Fix_geo'] = outputs['FixGeometries']['OUTPUT']
+
+        feedback.setCurrentStep(2)
+        if feedback.isCanceled():
+            return {}
+
+        # Feature filter
+        alg_params = {
+            'INPUT': 'Calculated_7416d0e6_ace3_4d6e_b873_c816de0c8799',
+            'OUTPUT_menor_a_11': parameters['Output_menor_a_11']
+        }
+        outputs['FeatureFilter'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Output_menor_a_11'] = outputs['FeatureFilter']['OUTPUT_menor_a_11']
+
+        feedback.setCurrentStep(3)
+        if feedback.isCanceled():
+            return {}
+
+        # Drop field(s)
+        alg_params = {
+            'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length'],
+            'INPUT': 'Calculated_c05faa99_f644_4d2e_bfe8_15894f73a4b4',
+            'OUTPUT': parameters['Wldsout']
+        }
+        outputs['DropFields'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Wldsout'] = outputs['DropFields']['OUTPUT']
+
+        feedback.setCurrentStep(4)
+        if feedback.isCanceled():
+            return {}
+
+        # Add autoincremental field
+        alg_params = {
+            'FIELD_NAME': 'GID',
+            'GROUP_FIELDS': [''],
+            'INPUT': outputs['FixGeometries']['OUTPUT'],
+            'MODULUS': 0,
+            'SORT_ASCENDING': True,
+            'SORT_EXPRESSION': '',
+            'SORT_NULLS_FIRST': False,
+            'START': 1,
+            'OUTPUT': parameters['Autoinc_id']
+        }
+        outputs['AddAutoincrementalField'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Autoinc_id'] = outputs['AddAutoincrementalField']['OUTPUT']
+
+        feedback.setCurrentStep(5)
+        if feedback.isCanceled():
+            return {}
+
+        # Field calculator clone
+        alg_params = {
+            'FIELD_LENGTH': 10,
+            'FIELD_NAME': 'lmn',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 2,  # String
+            'FORMULA': '"NAME_PROP"',
+            'INPUT': 'menor_a_11_e1da0e1b_c48d_45d8_85fe_5c23d0cd17ba',
+            'OUTPUT': parameters['Field_calc']
+        }
+        outputs['FieldCalculatorClone'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Field_calc'] = outputs['FieldCalculatorClone']['OUTPUT']
+        return results
+
+    def name(self):
+        return 'model1'
+
+    def displayName(self):
+        return 'model1'
+
+    def group(self):
+        return ''
+
+    def groupId(self):
+        return ''
+
+    def createInstance(self):
+        return Model1()

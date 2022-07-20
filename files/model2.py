31152fee3960f15b42359049998171109860ec41
CatalinaBanfi <108238497+CatalinaBanfi@users.noreply.github.com>
2022-07-19 23:29:23 -0300

    Add files via upload
    Adjunté archivos .py (obtenidos desde QGIS) sin ningún cambio

---
+++ files/model2.py
@@ -0,0 +1,132 @@
+"""
+Model exported as python.
+Name : modelo1
+Group : 
+With QGIS : 32208
+"""
+
+from qgis.core import QgsProcessing
+from qgis.core import QgsProcessingAlgorithm
+from qgis.core import QgsProcessingMultiStepFeedback
+from qgis.core import QgsProcessingParameterFeatureSink
+import processing
+
+
+class Modelo1(QgsProcessingAlgorithm):
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterFeatureSink('Autoinc', 'AUTOINC', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Wldsout', 'wldsout', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Length', 'length', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Field_calc', 'field_calc', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Output_menor_a_11', 'OUTPUT_menor_a_11', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Fixgeo', 'fix.geo', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+
+    def processAlgorithm(self, parameters, context, model_feedback):
+        # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
+        # overall progress through the model
+        feedback = QgsProcessingMultiStepFeedback(6, model_feedback)
+        results = {}
+        outputs = {}
+
+        # Corregir geometrías
+        alg_params = {
+            'INPUT': 'C:/Users/catal/OneDrive - Facultad de Cs Económicas - UBA/UdeSA/Herramientas computacionales/Clase 4/langa/langa.shp',
+            'OUTPUT': parameters['Fixgeo']
+        }
+        outputs['CorregirGeometras'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Fixgeo'] = outputs['CorregirGeometras']['OUTPUT']
+
+        feedback.setCurrentStep(1)
+        if feedback.isCanceled():
+            return {}
+
+        # Calculadora de campos clone
+        alg_params = {
+            'FIELD_LENGTH': 10,
+            'FIELD_NAME': 'lnm',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 2,  # Cadena
+            'FORMULA': '"NAME_PROP"',
+            'INPUT': 'menor_a_11_e2e59278_7b99_43cc_b719_245d3eee1cab',
+            'OUTPUT': parameters['Field_calc']
+        }
+        outputs['CalculadoraDeCamposClone'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Field_calc'] = outputs['CalculadoraDeCamposClone']['OUTPUT']
+
+        feedback.setCurrentStep(2)
+        if feedback.isCanceled():
+            return {}
+
+        # Quitar campo(s)
+        alg_params = {
+            'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length'],
+            'INPUT': 'Calculado_700dd975_e378_44ba_9587_6cc8f33365ee',
+            'OUTPUT': parameters['Wldsout']
+        }
+        outputs['QuitarCampos'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Wldsout'] = outputs['QuitarCampos']['OUTPUT']
+
+        feedback.setCurrentStep(3)
+        if feedback.isCanceled():
+            return {}
+
+        # Calculadora de campos
+        alg_params = {
+            'FIELD_LENGTH': 2,
+            'FIELD_NAME': 'length',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 1,  # Entero
+            'FORMULA': 'length(NAME_PROP)',
+            'INPUT': 'Incrementado_81c51bfe_63df_4568_b888_0225bddfe225',
+            'OUTPUT': parameters['Length']
+        }
+        outputs['CalculadoraDeCampos'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Length'] = outputs['CalculadoraDeCampos']['OUTPUT']
+
+        feedback.setCurrentStep(4)
+        if feedback.isCanceled():
+            return {}
+
+        # Filtro de entidad
+        alg_params = {
+            'INPUT': 'Calculado_8076269b_ce95_46ff_9a5a_ee36032d6f82',
+            'OUTPUT_menor_a_11': parameters['Output_menor_a_11']
+        }
+        outputs['FiltroDeEntidad'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Output_menor_a_11'] = outputs['FiltroDeEntidad']['OUTPUT_menor_a_11']
+
+        feedback.setCurrentStep(5)
+        if feedback.isCanceled():
+            return {}
+
+        # Agregar campo que auto-incrementa 
+        alg_params = {
+            'FIELD_NAME': 'GID',
+            'GROUP_FIELDS': [''],
+            'INPUT': outputs['CorregirGeometras']['OUTPUT'],
+            'MODULUS': 0,
+            'SORT_ASCENDING': True,
+            'SORT_EXPRESSION': '',
+            'SORT_NULLS_FIRST': False,
+            'START': 1,
+            'OUTPUT': parameters['Autoinc']
+        }
+        outputs['AgregarCampoQueAutoincrementa'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Autoinc'] = outputs['AgregarCampoQueAutoincrementa']['OUTPUT']
+        return results
+
+    def name(self):
+        return 'modelo1'
+
+    def displayName(self):
+        return 'modelo1'
+
+    def group(self):
+        return ''
+
+    def groupId(self):
+        return ''
+
+    def createInstance(self):
+        return Modelo1()
--- files/model3.py
+++ files/model3.py
@@ -15,52 +15,56 @@ import processing
 class Modelo3(QgsProcessingAlgorithm):
 
     def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterFeatureSink('Drop_fields_3', 'drop_fields_3', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Fix_geo_3', 'fix_geo_3', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Landq', 'landq', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Popd1800', 'popd1800', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
+        self.addParameter(QgsProcessingParameterFeatureSink('Popd1900', 'popd1900', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Popd2000', 'popd2000', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
 
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
-        feedback = QgsProcessingMultiStepFeedback(4, model_feedback)
+        feedback = QgsProcessingMultiStepFeedback(7, model_feedback)
         results = {}
         outputs = {}
 
-        # Guardar objetos vectoriales en archivo
+        # Estadísticas de zona
         alg_params = {
-            'DATASOURCE_OPTIONS': '',
-            'INPUT': 'Estadistica_zonal_5fa625aa_6f46_46a9_ae4a_fb9bcb452e63',
-            'LAYER_NAME': '',
-            'LAYER_OPTIONS': '',
-            'OUTPUT': 'C:/Users/catal/OneDrive - Facultad de Cs Económicas - UBA/UdeSA/Herramientas computacionales/Clase 4/output/rasterstatics.gpkg',
-            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+            'COLUMN_PREFIX': 'popd2000',
+            'INPUT': 'Campos_restantes_ab67611e_24e1_40ef_ac3f_ce096f92e095',
+            'INPUT_RASTER': 'popd_2000AD_ac642b7f_86ba_4d87_a40f_2ae470d699fa',
+            'RASTER_BAND': 1,
+            'STATISTICS': [2],  # Media
+            'OUTPUT': parameters['Popd2000']
         }
-        outputs['GuardarObjetosVectorialesEnArchivo'] = processing.run('native:savefeatures', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        outputs['EstadsticasDeZona'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Popd2000'] = outputs['EstadsticasDeZona']['OUTPUT']
 
         feedback.setCurrentStep(1)
         if feedback.isCanceled():
             return {}
 
-        # Guardar registro en archivo
+        # Corregir geometrías
         alg_params = {
-            'OUTPUT': 'C:/Users/catal/OneDrive - Facultad de Cs Económicas - UBA/UdeSA/Herramientas computacionales/Clase 4/output/raster_stata.csv',
-            'USE_HTML': False,
-            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+            'INPUT': 'C:/Users/catal/OneDrive - Facultad de Cs Económicas - UBA/UdeSA/Herramientas computacionales/Clase 4/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shx',
+            'OUTPUT': parameters['Fix_geo_3']
         }
-        outputs['GuardarRegistroEnArchivo'] = processing.run('native:savelog', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        outputs['CorregirGeometras'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Fix_geo_3'] = outputs['CorregirGeometras']['OUTPUT']
 
         feedback.setCurrentStep(2)
         if feedback.isCanceled():
             return {}
 
-        # Guardar objetos vectoriales en archivo
+        # Quitar campo(s)
         alg_params = {
-            'DATASOURCE_OPTIONS': '',
-            'INPUT': 'Estadistica_zonal_5fa625aa_6f46_46a9_ae4a_fb9bcb452e63',
-            'LAYER_NAME': '',
-            'LAYER_OPTIONS': '',
-            'OUTPUT': 'C:/Users/catal/OneDrive - Facultad de Cs Económicas - UBA/UdeSA/Herramientas computacionales/Clase 4/output/raster_statics.gpkg',
-            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+            'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','TLC','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','MAPCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','POP_YEAR','GDP_MD','GDP_YEAR','ECONOMY','INCOME_GRP','FIPS_10','ISO_A2','ISO_A2_EH','ISO_A3_EH','ISO_N3','ISO_N3_EH','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_ISO','ADM0_DIFF','ADM0_TLC','ADM0_A3_US','ADM0_A3_FR','ADM0_A3_RU','ADM0_A3_ES','ADM0_A3_CN','ADM0_A3_TW','ADM0_A3_IN','ADM0_A3_NP','ADM0_A3_PK','ADM0_A3_DE','ADM0_A3_GB','ADM0_A3_BR','ADM0_A3_IL','ADM0_A3_PS','ADM0_A3_SA','ADM0_A3_EG','ADM0_A3_MA','ADM0_A3_PT','ADM0_A3_AR','ADM0_A3_JP','ADM0_A3_KO','ADM0_A3_VN','ADM0_A3_TR','ADM0_A3_ID','ADM0_A3_PL','ADM0_A3_GR','ADM0_A3_IT','ADM0_A3_NL','ADM0_A3_SE','ADM0_A3_BD','ADM0_A3_UA','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','LABEL_X','LABEL_Y','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FA','NAME_FR','NAME_EL','NAME_HE','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_UK','NAME_UR','NAME_VI','NAME_ZH','NAME_ZHT','FCLASS_ISO','TLC_DIFF','FCLASS_TLC','FCLASS_US','FCLASS_FR','FCLASS_RU','FCLASS_ES','FCLASS_CN','FCLASS_TW','FCLASS_IN','FCLASS_NP','FCLASS_PK','FCLASS_DE','FCLASS_GB','FCLASS_BR','FCLASS_IL','FCLASS_PS','FCLASS_SA','FCLASS_EG','FCLASS_MA','FCLASS_PT','FCLASS_AR','FCLASS_JP','FCLASS_KO','FCLASS_VN','FCLASS_TR','FCLASS_ID','FCLASS_PL','FCLASS_GR','FCLASS_IT','FCLASS_NL','FCLASS_SE','FCLASS_BD','FCLASS_UA'],
+            'INPUT': 'Geometr_as_corregidas_7635a498_a661_4b89_9737_b9aa9d949988',
+            'OUTPUT': parameters['Drop_fields_3']
         }
-        outputs['GuardarObjetosVectorialesEnArchivo'] = processing.run('native:savefeatures', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        outputs['QuitarCampos'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Drop_fields_3'] = outputs['QuitarCampos']['OUTPUT']
 
         feedback.setCurrentStep(3)
         if feedback.isCanceled():
@@ -68,15 +72,59 @@ class Modelo3(QgsProcessingAlgorithm):
 
         # Estadísticas de zona
         alg_params = {
-            'COLUMN_PREFIX': 'popd2000',
-            'INPUT': 'Estadistica_zonal_c8d485a0_4f0c_4c1e_b34e_7fa868d02f36',
-            'INPUT_RASTER': 'popd_2000AD_0fdcf75c_7398_4061_8fa3_ee6bff263ca8',
+            'COLUMN_PREFIX': '_',
+            'INPUT': 'Campos_restantes_ab67611e_24e1_40ef_ac3f_ce096f92e095',
+            'INPUT_RASTER': 'landquality_fc714d1e_9304_4ce9_8fa1_5f399ffcbe0e',
             'RASTER_BAND': 1,
             'STATISTICS': [2],  # Media
-            'OUTPUT': parameters['Popd2000']
+            'OUTPUT': parameters['Landq']
         }
         outputs['EstadsticasDeZona'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Popd2000'] = outputs['EstadsticasDeZona']['OUTPUT']
+        results['Landq'] = outputs['EstadsticasDeZona']['OUTPUT']
+
+        feedback.setCurrentStep(4)
+        if feedback.isCanceled():
+            return {}
+
+        # Guardar registro en archivo
+        alg_params = {
+            'OUTPUT': 'C:/Users/catal/OneDrive - Facultad de Cs Económicas - UBA/UdeSA/Herramientas computacionales/Clase 4/output/raster_statics.csv',
+            'USE_HTML': False,
+            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+        }
+        outputs['GuardarRegistroEnArchivo'] = processing.run('native:savelog', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+
+        feedback.setCurrentStep(5)
+        if feedback.isCanceled():
+            return {}
+
+        # Estadísticas de zona
+        alg_params = {
+            'COLUMN_PREFIX': 'pop1800',
+            'INPUT': 'Campos_restantes_ab67611e_24e1_40ef_ac3f_ce096f92e095',
+            'INPUT_RASTER': 'popd_1800AD_4e3576d5_2aef_45a9_a767_3a357db5d6e9',
+            'RASTER_BAND': 1,
+            'STATISTICS': [2],  # Media
+            'OUTPUT': parameters['Popd1800']
+        }
+        outputs['EstadsticasDeZona'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Popd1800'] = outputs['EstadsticasDeZona']['OUTPUT']
+
+        feedback.setCurrentStep(6)
+        if feedback.isCanceled():
+            return {}
+
+        # Estadísticas de zona
+        alg_params = {
+            'COLUMN_PREFIX': 'popd1900',
+            'INPUT': 'Campos_restantes_ab67611e_24e1_40ef_ac3f_ce096f92e095',
+            'INPUT_RASTER': 'popd_1900AD_605dfe9d_6a92_4489_b8d4_2dfa901cc0a8',
+            'RASTER_BAND': 1,
+            'STATISTICS': [2],  # Media
+            'OUTPUT': parameters['Popd1900']
+        }
+        outputs['EstadsticasDeZona'] = processing.run('native:zonalstatisticsfb', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Popd1900'] = outputs['EstadsticasDeZona']['OUTPUT']
         return results
 
     def name(self):

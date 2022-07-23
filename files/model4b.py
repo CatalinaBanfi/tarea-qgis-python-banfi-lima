afaee05d3dc9212e55017d21004f6f17673756ac
Matías Lima <71034029+limamatias@users.noreply.github.com>
2022-07-22 22:20:12 -0300

    Update model4b.py
    Agrego descripciones y ordeno los pasos d) al f).

--- files/model4b.py
+++ files/model4b.py
@@ -37,6 +37,33 @@
         outputs['Centroids'] = processing.run('native:centroids', cts_dict, context=context, feedback=feedback, is_child_algorithm=True)
         results['Country_centroids'] = outputs['Centroids']['OUTPUT']
         
+        # d) Utilizamos la función add geometry attributes para obtener las coordenadas geográficas de los países del punto c).
+        aga1_dict = {
+            'CALC_METHOD': 0,  # Layer CRS
+            'INPUT': outputs['Centroids']['OUTPUT'],
+            'OUTPUT': parameters['Centroids_w_coord']
+        }
+        outputs['AddGeometryAttributes'] = processing.run('qgis:exportaddgeometrycolumns', aga1_dict, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Centroids_w_coord'] = outputs['AddGeometryAttributes']['OUTPUT']
+        
+        # e) Usamos el algoritmo drop fields para determinar los campos con los que queremos quedarnos de la layer de b).
+        df1_dict = {
+            'COLUMN': ['scalerank'],
+            'INPUT': outputs['FixGeometriesCoast']['OUTPUT'],
+            'OUTPUT': parameters['Coastout']
+        }
+        outputs['DropFieldsFixgeo_coast'] = processing.run('native:deletecolumn', df1_dict, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Coastout'] = outputs['DropFieldsFixgeo_coast']['OUTPUT']
+        
+        # f) Usamos el algoritmo drop fields para determinar los campos con los que queremos quedarnos de la layer de a).
+        df2_dict = {
+            'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','TLC','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','MAPCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','POP_YEAR','GDP_MD','GDP_YEAR','ECONOMY','INCOME_GRP','FIPS_10','ISO_A2','ISO_A2_EH','ISO_A3_EH','ISO_N3','ISO_N3_EH','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_ISO','ADM0_DIFF','ADM0_TLC','ADM0_A3_US','ADM0_A3_FR','ADM0_A3_RU','ADM0_A3_ES','ADM0_A3_CN','ADM0_A3_TW','ADM0_A3_IN','ADM0_A3_NP','ADM0_A3_PK','ADM0_A3_DE','ADM0_A3_GB','ADM0_A3_BR','ADM0_A3_IL','ADM0_A3_PS','ADM0_A3_SA','ADM0_A3_EG','ADM0_A3_MA','ADM0_A3_PT','ADM0_A3_AR','ADM0_A3_JP','ADM0_A3_KO','ADM0_A3_VN','ADM0_A3_TR','ADM0_A3_ID','ADM0_A3_PL','ADM0_A3_GR','ADM0_A3_IT','ADM0_A3_NL','ADM0_A3_SE','ADM0_A3_BD','ADM0_A3_UA','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','LABEL_X','LABEL_Y','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FA','NAME_FR','NAME_EL','NAME_HE','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_UK','NAME_UR','NAME_VI','NAME_ZH','NAME_ZHT','FCLASS_ISO','TLC_DIFF','FCLASS_TLC','FCLASS_US','FCLASS_FR','FCLASS_RU','FCLASS_ES','FCLASS_CN','FCLASS_TW','FCLASS_IN','FCLASS_NP','FCLASS_PK','FCLASS_DE','FCLASS_GB','FCLASS_BR','FCLASS_IL','FCLASS_PS','FCLASS_SA','FCLASS_EG','FCLASS_MA','FCLASS_PT','FCLASS_AR','FCLASS_JP','FCLASS_KO','FCLASS_VN','FCLASS_TR','FCLASS_ID','FCLASS_PL','FCLASS_GR','FCLASS_IT','FCLASS_NL','FCLASS_SE','FCLASS_BD','FCLASS_UA'],
+            'INPUT': outputs['FixGeometriesCountries']['OUTPUT'],
+            'OUTPUT': parameters['Centroidsout']
+        }
+        outputs['DropFieldsCentroids_w_coord'] = processing.run('native:deletecolumn', df2_dict, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Centroidsout'] = outputs['DropFieldsCentroids_w_coord']['OUTPUT']
+        
         # Field calculator - add_geo_coast
         alg_params = {
             'FIELD_LENGTH': 10,
@@ -100,15 +127,6 @@
         outputs['DropFieldsAdded_field_cent_lon'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Centroids_lan_lon_dropfields'] = outputs['DropFieldsAdded_field_cent_lon']['OUTPUT']
 
-        # Drop field(s) - fixgeo_coast
-        alg_params = {
-            'COLUMN': ['scalerank'],
-            'INPUT': 'Fixed_geometries_3590b732_8e19_4789_ac6d_c0cdd9f97228',
-            'OUTPUT': parameters['Coastout']
-        }
-        outputs['DropFieldsFixgeo_coast'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Coastout'] = outputs['DropFieldsFixgeo_coast']['OUTPUT']
-
         # Drop field(s) - nearest_cat_adjust
         alg_params = {
             'COLUMN': ['xcoord','ycoord'],
@@ -170,15 +188,6 @@
         outputs['FieldCalculatorCatAdjust'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Nearest_cat_adjust'] = outputs['FieldCalculatorCatAdjust']['OUTPUT']
 
-        # Drop field(s) - centroids_w_coord
-        alg_params = {
-            'COLUMN': ['featurecla','scalerank','LABELRANK','SOVEREIGNT','SOV_A3','ADM0_DIF','LEVEL','TYPE','TLC','ADM0_A3','GEOU_DIF','GEOUNIT','GU_A3','SU_DIF','SUBUNIT','SU_A3','BRK_DIFF','NAME','NAME_LONG','BRK_A3','BRK_NAME','BRK_GROUP','ABBREV','POSTAL','FORMAL_EN','FORMAL_FR','NAME_CIAWF','NOTE_ADM0','NOTE_BRK','NAME_SORT','NAME_ALT','MAPCOLOR7','MAPCOLOR8','MAPCOLOR9','MAPCOLOR13','POP_EST','POP_RANK','POP_YEAR','GDP_MD','GDP_YEAR','ECONOMY','INCOME_GRP','FIPS_10','ISO_A2','ISO_A2_EH','ISO_A3_EH','ISO_N3','ISO_N3_EH','UN_A3','WB_A2','WB_A3','WOE_ID','WOE_ID_EH','WOE_NOTE','ADM0_ISO','ADM0_DIFF','ADM0_TLC','ADM0_A3_US','ADM0_A3_FR','ADM0_A3_RU','ADM0_A3_ES','ADM0_A3_CN','ADM0_A3_TW','ADM0_A3_IN','ADM0_A3_NP','ADM0_A3_PK','ADM0_A3_DE','ADM0_A3_GB','ADM0_A3_BR','ADM0_A3_IL','ADM0_A3_PS','ADM0_A3_SA','ADM0_A3_EG','ADM0_A3_MA','ADM0_A3_PT','ADM0_A3_AR','ADM0_A3_JP','ADM0_A3_KO','ADM0_A3_VN','ADM0_A3_TR','ADM0_A3_ID','ADM0_A3_PL','ADM0_A3_GR','ADM0_A3_IT','ADM0_A3_NL','ADM0_A3_SE','ADM0_A3_BD','ADM0_A3_UA','ADM0_A3_UN','ADM0_A3_WB','CONTINENT','REGION_UN','SUBREGION','REGION_WB','NAME_LEN','LONG_LEN','ABBREV_LEN','TINY','HOMEPART','MIN_ZOOM','MIN_LABEL','MAX_LABEL','LABEL_X','LABEL_Y','NE_ID','WIKIDATAID','NAME_AR','NAME_BN','NAME_DE','NAME_EN','NAME_ES','NAME_FA','NAME_FR','NAME_EL','NAME_HE','NAME_HI','NAME_HU','NAME_ID','NAME_IT','NAME_JA','NAME_KO','NAME_NL','NAME_PL','NAME_PT','NAME_RU','NAME_SV','NAME_TR','NAME_UK','NAME_UR','NAME_VI','NAME_ZH','NAME_ZHT','FCLASS_ISO','TLC_DIFF','FCLASS_TLC','FCLASS_US','FCLASS_FR','FCLASS_RU','FCLASS_ES','FCLASS_CN','FCLASS_TW','FCLASS_IN','FCLASS_NP','FCLASS_PK','FCLASS_DE','FCLASS_GB','FCLASS_BR','FCLASS_IL','FCLASS_PS','FCLASS_SA','FCLASS_EG','FCLASS_MA','FCLASS_PT','FCLASS_AR','FCLASS_JP','FCLASS_KO','FCLASS_VN','FCLASS_TR','FCLASS_ID','FCLASS_PL','FCLASS_GR','FCLASS_IT','FCLASS_NL','FCLASS_SE','FCLASS_BD','FCLASS_UA'],
-            'INPUT': 'Added_geom_info_448a073e_06ab_4742_849e_cb1a0ff58d5f',
-            'OUTPUT': parameters['Centroidsout']
-        }
-        outputs['DropFieldsCentroids_w_coord'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Centroidsout'] = outputs['DropFieldsCentroids_w_coord']['OUTPUT']
-
         # Drop field(s) - added_field_coast_lon
         alg_params = {
             'COLUMN': ['xcoord','ycoord'],
@@ -238,13 +247,4 @@
         outputs['Vdistance'] = processing.run('grass7:v.distance', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Distout'] = outputs['Vdistance']['output']
         results['Nearout'] = outputs['Vdistance']['from_output']
-
-        # Add geometry attributes
-        alg_params = {
-            'CALC_METHOD': 0,  # Layer CRS
-            'INPUT': outputs['Centroids']['OUTPUT'],
-            'OUTPUT': parameters['Centroids_w_coord']
-        }
-        outputs['AddGeometryAttributes'] = processing.run('qgis:exportaddgeometrycolumns', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Centroids_w_coord'] = outputs['AddGeometryAttributes']['OUTPUT']
-       
+      
